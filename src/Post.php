b930ef99dd5e02fee8dd4e3059e2d91858dfcf41
Lucifer <cuongthinhtuan2006@gmail.com>
2020-05-29 21:37:04 +0700

    Allowing for Package Expansion Part 1

--- src/Post.php
+++ src/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
+
+    public function extra($field)
+    {
+        return optional(json_decode($this->extra))->$field;
+    }
 }
--- src/Repositories/PostRepository.php
+++ src/Repositories/PostRepository.php
@@ -17,7 +17,15 @@ class PostRepository
             'slug' => Str::slug($post['title']),
             'title' => $post['title'],
             'body' => $post['body'],
-            'extra' => $post['extra'] ?? json_encode([]),
+            'extra' => $this->extra($post),
         ]);
+    }
+
+    private function extra($post)
+    {
+        $extra = (array) json_decode($post['extra'] ?? '[]');
+        $attributes = array_except($post, ['title', 'body', 'identifier', 'extra']);
+
+        return json_encode(array_merge($extra, $attributes));
     }
 }
